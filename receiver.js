19332ff5076ada4ab5afa9ebed3ed30df8471899
swarnaadeepjena <swarnadeep.jena@goodvets.com>
2023-06-22 17:08:32 +0530

    dd

---
+++ receiver.js
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+const { default: axios } = require("axios");
+const sqs = new AWS.SQS();
+const ssm = new AWS.SSM();
+
+module.exports.sqsReciveHandler = async (event) => {
+  console.log(event);
+  try {
+    const parameters = {
+    Names: ['/Dev-lambda-EZ-API/API_KEY','/Dev-lambda-EZ-API/receiver-api-url'] 
+    
+  };
+    const response = await ssm.getParameters(parameters).promise();
+    const values = response.Parameters.map(parameter => parameter.Value);
+    console.log(values)
+    const config = {
+      headers: { apikey: values[0] },
+    };
+    if (event.Records.length) {
+      const syncLocationPromise = event.Records.map((element) => {
+         const url =  values[1]+element.body
+        return axios.post(url, {}, config);
+      });
+      const returnData = await Promise.all(syncLocationPromise);
+      console.log(returnData);
+    }
+  } catch (err) {
+    console.error("Error receiving messages:", err);
+    throw err;
+  }
+};
---
+++ sender.js
@@ -0,0 +1,43 @@
+const AWS = require("aws-sdk");
+const axios = require("axios");
+const sqs = new AWS.SQS();
+const dotenv = require("dotenv")
+const ssm = new AWS.SSM();
+dotenv.config()
+
+ module.exports.sqsSendHandler = async (event) => {
+
+
+
+  try {
+         const parameters = {
+    Names: ['/Dev-lambda-EZ-API/API_KEY','/Dev-lambda-EZ-API/sender-api-url','/Dev-lambda-EZ-API/sqs-url'] 
+    
+  };
+    const response = await ssm.getParameters(parameters).promise();
+    const values = response.Parameters.map(parameter => parameter.Value);
+    console.log(values)
+    let params = {};
+    const config = {
+      headers: { apikey: values[0] },
+    };
+    const url = values[1];
+    const locationList = await axios.get(url, config);
+    if (locationList.data.response.length) {
+    const messageArraySQS = locationList.data.response.map((element) => {
+        params = {
+          MessageBody: element.idLocation,
+          QueueUrl: values[2],
+        };
+        return sqs.sendMessage(params).promise();
+        //console.log("Message sent:", data.MessageId);
+      });
+    const returnData = await Promise.all(messageArraySQS);
+    console.log(returnData);
+      //return data.MessageId;
+    }
+  } catch (error) {
+    console.error("Error sending message:", error);
+    throw error;
+  }
+ };
